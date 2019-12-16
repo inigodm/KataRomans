69cf9416d37f2b96798cd0e58ef4466bdf10a1a5
Unknown <unknown@example.com>
2019-12-16 17:56:54 +0100

    red

--- test/com/inigo/tdd/RomansWorksIf.java
+++ test/com/inigo/tdd/RomansWorksIf.java
@@ -1,12 +1,15 @@
 package com.inigo.tdd;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class RomansWorksIf {
 	@Test
-	public void environment_is_ok() {
-		assertTrue(true);
+	public void given_zero_then_returns_void() {
+		int arabic = 0;
+		String roman = Romans.toRoman(arabic);
+		assertEquals("", roman);
 	}
 }
