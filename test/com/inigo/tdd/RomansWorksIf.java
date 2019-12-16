fdb72708a8aa9ff13546762845477e0fe1f6f3f7
inigo delgado <inigo.delgado@gmail.com>
2019-12-16 17:54:30 +0100

    Environment

---
+++ test/com/inigo/tdd/RomansWorksIf.java
@@ -0,0 +1,12 @@
+package com.inigo.tdd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RomansWorksIf {
+	@Test
+	public void environment_is_ok() {
+		assertTrue(true);
+	}
+}
